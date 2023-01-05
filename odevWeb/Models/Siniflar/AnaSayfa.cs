db68138c5679a81ab725335c2a38b995ef891062
sena <snbyrktr@hotmail.com>
2023-01-06 00:35:26 +0300

    sql bağlandi

---
+++ odevWeb/Models/Siniflar/AnaSayfa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace odevWeb.Models.Siniflar
+{
+    public class AnaSayfa
+    {
+        [Key]
+        public int id { get; set; }
+        public string profil { get; set; }
+        public string isim { get; set; }
+        public string unvan { get; set; }
+        public string iletisim { get; set; }
+    }
+}
---
+++ odevWeb/Models/Siniflar/ikonlar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace odevWeb.Models.Siniflar
+{
+    public class ikonlar
+    {
+        [Key]
+        public int id { get; set; }
+        public string ikon { get; set; }
+        public string link { get; set; }
+    }
+}
