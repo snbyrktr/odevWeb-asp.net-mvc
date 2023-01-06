343dab489ae2e1a0b0f74ac1b2060c4186d2b92e
sena <snbyrktr@hotmail.com>
2023-01-06 03:29:12 +0300

    bitti

--- odevWeb/Controllers/AdminController.cs
+++ odevWeb/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using odevWeb.Models.Siniflar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,33 @@ namespace odevWeb.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
+        Context c = new Context();
+        [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var deger = c.AnaSayfas.ToList();
+            return View(deger);
+        }
+        public ActionResult AnaSayfaGetir(int id)
+        {
+            var ag = c.AnaSayfas.Find(id);
+            return View("AnaSayfaGetir", ag);
+        }
+        public ActionResult AnaSayfaGuncelle(AnaSayfa x)
+        {
+            var ag = c.AnaSayfas.Find(x.id);
+            ag.isim = x.isim;
+            ag.profil = x.profil;
+            ag.iletisim = x.iletisim;
+            ag.unvan = x.unvan;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+         
+        public ActionResult ikonListesi()
+        {
+            var deger = c.ikonlars.ToList();
+            return View(deger);
         }
     }
 }
